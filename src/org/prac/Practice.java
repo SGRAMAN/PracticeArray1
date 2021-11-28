848ccdde9ec5b3e7777f893819cbd0eaf0e4d1f5
SGRAMAN <sgraman.subramanian@gmail.com>
2021-11-28 19:01:28 +0530

    message

---
+++ src/org/prac/Practice.java
@@ -0,0 +1,63 @@
+package org.prac;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+	
+		List a = new LinkedList();
+		a.add(100);
+		a.add(200);
+		a.add(300);
+		a.add(400);
+		a.add(500);
+		a.add(600);
+		a.add(700);
+		
+		int size = a.size();
+		System.out.println(size);
+		
+		Object remove = a.remove(5);
+		System.out.println(remove);
+		
+		for (int i = 0; i < a.size(); i++) {
+			Object object = a.get(i);
+			System.out.println(object);
+			
+			
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+		}
+
+			
+		}
+	
+		
+		
+		
+		
+		
+	
+		
+		
+		
+	
+	
+
+
